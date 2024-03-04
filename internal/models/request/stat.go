976833e5632f6f7dfaa80c40a497ccd8aaa56c79
Thanwa Chokporntaveesuk <rewphg@thanwas-macbook-pro.tail3bbfc.ts.net>
2024-03-04 13:25:05 +0700

    fix json binding tag

--- internal/models/request/stat.go
+++ internal/models/request/stat.go
@@ -13,7 +13,7 @@ type StatCollectionRequest struct {
 	Duration            int                            `json:"duration"` //capture duration in second
 	PollingRate         int                            `json:"polling_rate"`
 	StatCollectionStage constants.DataCollectionStages `json:"stat_collection_stage"`
-	Direction           constants.Direction            `json:"direction"`
+	Direction           constants.Direction            `json:"selected_direction"`
 	StartedAt           time.Time                      `json:"started_at"`
 	EndedAt             time.Time                      `json:"ended_at"`
 	CreatedAt           time.Time                      `json:"created_at"`
